bb7d2dd42561ab2a64f0e11cc02cb98ed9aae80f
ankitpatel441 <ankitpatel441020@gmail.com>
2026-06-01 15:46:25 +0530

    Add practice questions and examples for Day 8, including array methods and destructuring

--- day8/PracticeQuestions.js
+++ day8/PracticeQuestions.js
@@ -1,3 +1,72 @@
 // Day 8 - Practice Questions
 // Add your practice questions and solutions below
 
+// Practice Qs
+// Check if all numbers in our array are multiples of 10 or not.
+
+// let nums = [10, 20, 30, 40];
+
+// let ans = nums.every((el) => el % 10 == 0);
+
+// console.log(ans);
+
+//  Practice Qs
+//Create a function to find the min number in an array.
+
+// let nums = [2, 3, 4, 6, 6, 7, 8, 9, 1 ];
+
+// let ans = nums.reduce((min, el) => {
+//     if(min < el)  {
+//         return min;
+//     } else  {
+//         return el;
+//     }
+// })
+
+// console.log(ans)
+
+// function sum() {
+//     console.log(arguments)
+// }
+
+// function sum(...args) {
+//     return args.reduce((sum, el) => sum +  el); 
+// }
+
+// // array destructuring
+// let name = ["anki", "akki", "harsh", "param", "aman", "ruchi"];
+
+// let [winner, runnerup, secrunnrup] = name;
+
+// console.log(winner);
+// console.log(runnerup);
+// console.log(secrunnrup);
+
+// how to work destructuring
+let name = ["ankit", "akki", "harsh", "param", "aman", "ruchi"];
+
+// let winner = name[0];
+// let runnerup = name[1];
+// let secrunnrup = name[2];
+
+let [winner, runnerup, ...others] = name;
+
+console.log(winner);   // ankit
+console.log(runnerup); // akki
+console.log(others);
+
+//Object Destructuring
+const student = {
+    name: "Akki",
+    age: 21,
+    city: "Raipur"
+};
+
+let { name, age, city } = student;
+
+console.log(name); // Akki
+console.log(age);  // 21
+console.log(city); // Raipur
+
+
+
--- day8/day8.js
+++ day8/day8.js
@@ -1,3 +1,177 @@
 // JavaScript - Day 8
 // Add your code here
 
+// Array meethods
+// forEach, map ,  filtersome , every,  reduce
+
+// //forEach
+// let arr = [1, 2, 3, 4, 5];
+
+// //arr func
+// arr.forEach((el) => {
+//     console.log(el)
+// })
+
+// // regl func
+// arr.forEach(function(el) {
+//     console.log(el)
+// })
+
+
+// let print = function(el) {
+//     console.log(el)
+// }
+
+// arr.forEach(print)
+
+// let arr =  [{
+//     name: "ankit",
+//     markes: 95.4,
+// }, 
+// {
+//     name: "sumu",
+//     markes: 75.4,
+
+// }, 
+// {
+//     name: "harsh",
+//     markes: 65.3,
+// }]
+
+// arr.forEach((el) => {
+//     console.log(el.markes);
+// });
+
+// //map
+// let num = [1, 2, 3, 4, 5];
+
+// let double = num.map((el) => {
+//     return el * 2;
+// })
+
+// let students =  [{
+//     name: "ankit",
+//     markes: 95.4,
+// }, 
+// {
+//     name: "sumu",
+//     markes: 75.4,
+
+// }, 
+// {
+//     name: "harsh",
+//     markes: 65.3,
+// }]
+
+// let gpa = students.map((el) =>{
+//     return el.markes  / 10;
+// })
+
+//filter 
+// let num = [2, 3, 4, 5, ,6 ,7 ,8 , 9];
+
+// let even = num.filter((el) => {
+    // return el % 2 != 0;
+//     return el > 5;
+// })
+
+// //every
+// let num = [2, 4, 6, 8].every((el) => {
+//     return el % 2 === 0;
+// });
+
+// console.log(num); //true
+
+// let num2 = [2, 4, 6, 8, 9].every((el) => {
+//     return el % 2 !== 0;
+// });
+
+// console.log(num2); // false
+
+//some 
+// [2, 4, 6, 8, 9].some(el => el % 2 !== 0); 
+// // true
+
+// // reduce
+// let num = [2, 4, 6 ,8];
+// let finalVal = num.reduce((res, el) => {
+//  return   (res + el)
+// });
+
+// console.log(finalVal);
+
+//funding maximum in an array 
+
+// let nums = [2, 3, 4, 6, 6, 7, 8, 9, 1 ];
+
+// let max =  -1;
+
+// for(let i=0; i<nums.length; i++) {
+//     if(max < nums[i]) {
+//         max = nums[i]
+//     }
+// }
+
+// console.log(max);
+
+// let max = nums.reduce((max, el) => {
+//     if(max < el) {
+//         return el;
+//     } else {
+//         return max;
+//     }
+// })
+
+// console.log(max)
+
+// Default Parameters
+// function ret(a, b=2){
+//    return a + b;
+// }
+
+// // ret(3) // 5
+
+// function ret2(a = 2, b){
+//    return a + b; 
+// }
+
+// //ret2(2) // a=2 , b =  undefind
+
+
+//spread 
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// // console.log(Math.min(...nums));
+// console.log(..."codewithakki")
+
+// //spread array literals
+// let arr = [1, 3, 4, 6, 7, 8, 9];
+
+// let newArr = [...arr];
+
+// console.log(newArr);
+
+// let chat = [... "hello"];
+// console.log(chat)
+
+// //spread object literals
+// let data = {
+//     email: "ankit@gmail.com",
+//     password: "pass@123"
+// }
+// let newData = {...data, id: 123, username: "akki20"};
+
+// console.log(newData);
+
+// let arr = [1, 3, 4, 5, 6, 7, 8]; //value
+// let obj1 = {...data} //obj1 -> key:>val
+
+
+// rest
+
+function sum (...args) {
+    for(let i = 0; i<args.length; i++) {
+        console.log("you gave us : ", args[i]);
+    }
+}
+
