b2f1066005abe2d8df9644f4deedc34b2731b3d7
ankitpatel441 <ankitpatel441020@gmail.com>
2026-06-03 11:52:04 +0530

    Add practice questions for Day 9, including DOM manipulation examples

--- JavaScript/day9/PracticeQuestions.js
+++ JavaScript/day9/PracticeQuestions.js
@@ -1,3 +1,40 @@
 // Day 9 - Practice Questions
-// Add your practice questions and solutions below
+
+// Practice Qs
+// Add the following elements to the container using only JavaScript and the DOM methods.
+
+// i) a <p> with red text that says "Hey I'm red!"
+let para1 = document.createElement("p");
+para1.innerText = "Hey I'm red!";
+para1.style.color = "red";
+document.querySelector("body").append(para1)
+
+
+// ii) an <h3> with blue text that says "I'm a blue h3!"
+let h3 = document.createElement("h3");
+h3.innerText = "I'm a blue h3!";
+h3.style.color = "blue";
+document.querySelector("body").append(h3)
+
+// iii) a <div> with a black border and pink background color with the following elements inside of it:
+// . another <h1> that says "I'm in a div"
+// . a <p> that says "ME TOO!"
+
+let div = document.createElement("div")
+let h1 = document.createElement("h1")
+let para2 = document.createElement("p")
+
+div.style.border = "2px solid black";
+div.style.backgroundColor = "pink"
+
+h1.innerText = "I'm  in a div"
+para2.innerText = "ME TOO"
+
+div.append(h1)
+div.append(para2)
+
+document.body.append(div);
+
+
+
 
--- JavaScript/day9/day9.js
+++ JavaScript/day9/day9.js
@@ -44,13 +44,117 @@
 // console.dir(heading.style.color = "green")
 // console.dir(heading.style.backgroundColor = "yellow")
 
-let links = document.querySelectorAll(".box a");
+// let links = document.querySelectorAll(".box a");
 
 // for(let i = 0; i < links.length; i++) {
 //     links[i].style.color = "yellow";
 // }
 
+// for(let link of links) {
+//     link.style.color = "yellow";
+// }
 
-for(let link of links) {
-    link.style.color = "yellow";
-}
+//classList - add
+// let heading = document.querySelector("h1");
+// heading.classList.add("green")
+
+//classList - remove
+// heading.classList.remove("green")
+
+//classList - contains
+// heading.classList.contains("green")
+
+//classList - toggle no-->yes, yes-->no
+// heading.classList.toggle("green")
+
+// // Navigation
+// let box = document.querySelector(".box");
+
+// // parent element
+// console.log(box.parentElement);
+
+// // children
+// console.log(box.children);
+
+// // number of children
+// console.log(box.childElementCount);
+
+// // previous sibling
+// console.log(box.previousElementSibling);
+
+// // next sibling
+// console.log(box.nextElementSibling);
+
+//Adding Elements in DOM
+// createElement
+// let para = document.createElement("p");
+// para.innerText = "I am a new paragraph";
+// console.log(para)
+
+//appendChild
+// let box = document.querySelector(".box");
+
+// let para = document.createElement("p");
+// para.innerText = "I am a new paragraph";
+
+// box.appendChild(para);
+
+//append
+// let box = document.querySelector(".box");
+
+// let btn = document.createElement("button");
+// btn.innerText = "Click Me";
+
+// box.append("do not click thix btn");
+// box.append(btn);
+
+//prepend
+// let box = document.querySelector(".box");
+
+// let heading = document.createElement("h3");
+// heading.innerText = "Spider-Man Info";
+
+// box.prepend(heading);
+
+// //insertAdjacentElement - beforebegin , afterbegin , beforeend , afterend
+// let box = document.querySelector(".box");
+
+// let btn1 = document.createElement("button");
+// btn1.innerText = "beforebegin";
+
+// let btn2 = document.createElement("button");
+// btn2.innerText = "afterbegin";
+
+// let btn3 = document.createElement("button");
+// btn3.innerText = "beforeend";
+
+// let btn4 = document.createElement("button");
+// btn4.innerText = "afterend";
+
+// box.insertAdjacentElement("beforebegin", btn1);
+// box.insertAdjacentElement("afterbegin", btn2);
+// box.insertAdjacentElement("beforeend", btn3);
+// box.insertAdjacentElement("afterend", btn4);
+
+// removing Elements
+// remove()
+// let box = document.querySelector(".box");
+
+// let btn = document.createElement("button");
+// btn.innerText = "Click Me";
+
+// box.append(btn);
+
+// // Remove button
+// btn.remove();
+
+// // removeChild()
+// let box = document.querySelector(".box");
+
+// let btn = document.createElement("button");
+// btn.innerText = "Click Me";
+
+// box.append(btn);
+
+// // Remove button using parent
+// box.removeChild(btn);
