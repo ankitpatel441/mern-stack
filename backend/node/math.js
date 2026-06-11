c7bfc94a623770b17a2c8344056fd0997a129e13
ankitpatel441 <ankitpatel441020@gmail.com>
2026-06-11 08:22:01 +0530

    backend node.js

---
+++ backend/node/math.js
@@ -0,0 +1,26 @@
+// module.exports and require
+
+// module.exports.sum = (a, b) => a+b;
+// module.exports.mul = (a, b) => a*b;
+// module.exports.PI = 3.14;
+// module.exports.g = 9.8;
+
+// const sum = (a, b) => a+b;
+// const mul = (a, b) => a*b
+// const PI = 3.14;
+// const g = 9.8;
+
+// let obj = {
+//     sum:sum,
+//     mul: mul,
+//     g: g,
+//     PI:PI,
+// };
+
+// module.exports = obj;
+
+//inport
+export const sum = (a, b) => a+b;
+export const mul = (a, b) => a*b;
+export const PI = 3.14;
+export const g = 9.8;
---
+++ backend/node/server.js
@@ -0,0 +1,22 @@
+// process.argv and process
+// console.log(process.argv);
+// console.log(process.argv);
+
+// reauiring file
+// const math = require("./math");
+// console.log(math);
+
+// reauiring directories
+// const fruits = require("./Fruits");
+
+// console.log(fruits);
+
+//import
+
+import {sum, PI} from "./math.js"
+
+console.log(sum(1, 2));
+
+
+
+
