2f05efad073f2c3ac2d300e3254a7dc3ea7502fe
ankitpatel441 <ankitpatel441020@gmail.com>
2026-06-12 10:31:31 +0530

    Add EJS templating with basic server setup
    - Created package.json with dependencies for EJS and Express.
    - Implemented server.js to set up an Express server with EJS as the view engine.
    - Added home.ejs view for the home page with a simple welcome message and a button.
    - Created rolldice.ejs view to display a random dice value.

---
+++ backend/ejs/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/rolldice", (req, res) => {
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", { diceVal });
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
