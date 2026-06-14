faee1ed7e610423571378d52cc2a8e28b1b27fe0
ankitpatel441 <ankitpatel441020@gmail.com>
2026-06-14 08:55:38 +0530

    Add EJS views and static assets for Instagram clone, including profile, posts, and navigation

--- backend/ejs/server.js
+++ backend/ejs/server.js
@@ -4,11 +4,24 @@ const path = require("path");
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,"pubilc")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home");
+});
+
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+  const instaData = require("./data.json")
+    let data = instaData[username];
+
+    if (data) {
+        res.render("instagram", { data });
+    } else {
+        res.send(`<h1>Account not found!</h1>`);
+    }
 });
 
 app.get("/rolldice", (req, res) => {
